cdd071677164f00f611eba4db55f48e0e9d58757
LorenG08 <159853432+LorenG08@users.noreply.github.com>
2025-08-24 20:16:27 +0800

    MainActivity.kt

--- RGB/MainActivity.kt
+++ RGB/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rgbproject
+package com.example.pagonrgb
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
@@ -13,38 +13,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+      
         val redEditText: EditText = findViewById(R.id.redEditText)
         val greenEditText: EditText = findViewById(R.id.greenEditText)
         val blueEditText: EditText = findViewById(R.id.blueEditText)
         val convertButton: Button = findViewById(R.id.convertButton)
         val colorView: View = findViewById(R.id.colorView)
 
-
+        
         convertButton.setOnClickListener {
-
+           
             val redHex = redEditText.text.toString()
             val greenHex = greenEditText.text.toString()
             val blueHex = blueEditText.text.toString()
 
-
+           
             if (redHex.length != 2 || greenHex.length != 2 || blueHex.length != 2) {
                 Toast.makeText(this, "Please enter a two-digit code for each color.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             try {
-
+                
                 val red = redHex.toInt(16)
                 val green = greenHex.toInt(16)
                 val blue = blueHex.toInt(16)
 
-
+              
                 val newColor = Color.rgb(red, green, blue)
                 colorView.setBackgroundColor(newColor)
 
             } catch (e: NumberFormatException) {
-
+                
                 Toast.makeText(this, "Invalid characters. Use 0-9 or A-F.", Toast.LENGTH_SHORT).show()
             }
         }
