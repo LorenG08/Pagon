4d50399ef425ffaf2273ed045079e06da812ed33
LorenG08 <159853432+LorenG08@users.noreply.github.com>
2025-12-13 08:44:47 +0800

    Add files via upload

---
+++ FinalProject_PathToMerit_Pagon/god.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+@export var dialogue_path: String ="res://dialogues/god.dialogue"
+
+
+
+var player_in_area = false
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_exited", Callable(self, "_on_body_exited"))
+	
+
+
+func _on_body_entered(body):
+	if body.name == "player":
+		player_in_area = true
+		print(Global.donation_counter)
+
+func _on_body_exited(body):
+	if body.name == "player":
+		player_in_area = false
+
+
+func _process(delta):
+	if player_in_area and Input.is_action_just_pressed("interact"):
+		Global.area_entered = true
+		DialogueManager.show_dialogue_balloon(load(dialogue_path))
+	if Global.volunteer1 == true:
+		queue_free()
---
+++ FinalProject_PathToMerit_Pagon/tree2.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+@export var dialogue_path: String ="res://dialogues/tree1.dialogue"
+
+var player_in_area = false
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_exited", Callable(self, "_on_body_exited"))
+	
+
+
+func _on_body_entered(body):
+	if body.name == "player":
+		player_in_area = true
+
+
+
+func _on_body_exited(body):
+	if body.name == "player":
+		player_in_area = false
+
+
+func _process(delta):
+	if player_in_area and Input.is_action_just_pressed("interact"):
+		Global.area_entered = true
+		DialogueManager.show_dialogue_balloon(load(dialogue_path))
+	if Global.cut1_tree == true:
+		queue_free()
